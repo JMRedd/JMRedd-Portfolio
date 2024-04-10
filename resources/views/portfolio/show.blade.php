5deacba6dca20c455bf37cfb05a7b717ac0d54d1
Brandon Simmons <brandonsimmons992011@gmail.com>
2024-04-10 15:39:58 -0400

    Fixed mobile view

--- resources/views/portfolio/show.blade.php
+++ resources/views/portfolio/show.blade.php
@@ -16,59 +16,59 @@
         </div>
     </div>
 
-    <div class="mt-12 w-3/4">
+    <div class="mt-12 w-3/4 mb-20">
         <div class="mt-10 text-zinc-300">
             @if(isset($game->project_status))
-            <div class="flex items-center mb-4">
+            <div class="md:flex items-center mb-4">
                 <h3 class="mr-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Project Status:</h3>
                 <p class="mr-2 text-xl font-extrabold">{{ $game->project_status }}</p>
             </div>
             @endif
                 @if(isset($game->project_type))
-            <div class="flex items-center mb-4">
+            <div class="md:flex items-center mb-4">
                 <h3 class="mr-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Project Type:</h3>
                 <p class="mr-2 text-xl font-extrabold">{{ $game->project_type }}</p>
             </div>
                 @endif
                 @if(isset($game->project_duration))
-            <div class="flex items-center mb-4">
+            <div class="md:flex items-center mb-4">
                 <h3 class="mr-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Project Duration:</h3>
                 <p class="mr-2 text-xl font-extrabold">{{ $game->project_duration }}</p>
             </div>
                 @endif
                 @if(isset($game->software_used))
-            <div class="flex items-center mb-4">
+            <div class="md:flex items-center mb-4">
                 <h3 class="mr-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Software Used:</h3>
                 <p class="mr-2 text-xl font-extrabold">{{ $game->software_used }}</p>
             </div>
                 @endif
                 @if(isset($game->languages_used))
-            <div class="flex items-center mb-4">
+            <div class="md:flex items-center mb-4">
                 <h3 class="mr-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Languages Used:</h3>
                 <p class="mr-2 text-xl font-extrabold">{{ $game->languages_used }}</p>
             </div>
                 @endif
                 @if(isset($game->primary_roles))
-            <div class="flex items-center mb-4">
+            <div class="md:flex items-center mb-4">
                 <h3 class="mr-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Primary Role(s):</h3>
                 <p class="mr-2 text-xl font-extrabold">{{ $game->primary_roles }}</p>
             </div>
                 @endif
             <br>
                 @if(isset($game->file_path))
-                <div class="flex items-center mr-8">
+                <div class="md:flex items-center mr-8">
                     <h3 class="mr-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 italic">Download Files...</h3>
                     <p class="mr-2 text-xl font-extrabold"><a href="{{ $game->file_path }}" target="_blank">LINK</a></p>
                 </div>
                 @endif
                     @if(isset($game->itch_link))
-                <div class="flex items-center mr-8">
+                <div class="md:flex items-center mr-8">
                     <h3 class="mr-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 italic">Visit the ITCH.IO Page...</h3>
                     <p class="mr-2 text-xl font-extrabold"><a href="{{ $game->itch_link }}" target="_blank">LINK</a></p>
                 </div>
                     @endif
                     @if(isset($game->steam_link))
-                <div class="flex items-center">
+                <div class="md:flex items-center">
                     <h3 class="mr-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 italic">Visit the STEAM Page...</h3>
                     <p class="mr-2 text-xl font-extrabold"><a href="{{ $game->steam_link }}" target="_blank">LINK</a></p>
                 </div>
